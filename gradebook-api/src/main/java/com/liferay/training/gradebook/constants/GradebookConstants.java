6700530aa1aec5916d4baebf3b0dc5db234a0bca
tahmidashraf <62513943+tahmidashraf@users.noreply.github.com>
2023-02-06 11:31:20 +0600

    Add files via upload

---
+++ gradebook-api/src/main/java/com/liferay/training/gradebook/constants/GradebookConstants.java
@@ -0,0 +1,4 @@
+package com.liferay.training.gradebook.constants;
+ public class GradebookConstants {
+ public static final String RESOURCE_NAME = "com.liferay.training.gradebook";
+}
---
+++ gradebook-service/bin/main/META-INF/sql/tables.sql
@@ -0,0 +1,17 @@
+create table Gradebook_Assignment (
+	uuid_ VARCHAR(75) null,
+	assignmentId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	description STRING null,
+	dueDate DATE null,
+	status INTEGER,
+	statusByUserId LONG,
+	statusByUserName VARCHAR(75) null,
+	statusDate DATE null,
+	title STRING null
+);
