d6665868cba58da3fec41d02870ce08016a3b3f5
tahmidashraf <62513943+tahmidashraf@users.noreply.github.com>
2023-02-06 11:34:02 +0600

    Add files via upload

---
+++ src/main/java/com/liferay/training/gradebook/constants/GradebookConstants.java
@@ -0,0 +1,9 @@
+package com.liferay.training.gradebook.constants;
+
+public class GradebookConstants {
+
+	private static String GRADEBOOK_URL;
+	public static final String RESOURCE_NAME = GRADEBOOK_URL = "GRADEBOOK";
+	
+
+}
---
+++ src/main/java/com/liferay/training/gradebook/web/constants/GradebookPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.gradebook.web.constants;
+
+/**
+ * @author Asus
+ */
+public class GradebookPortletKeys {
+
+	public static final String GRADEBOOK =
+		"com_liferay_training_gradebook_web_portlet_GradebookPortlet";
+
+}
